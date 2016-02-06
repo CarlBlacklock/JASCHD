edce7f3f483d1c642fe13ef269a6660c80837ed2
CarlBlacklock <carl.alex.blacklock@gmail.com>
2016-02-06 04:23:59 -0500

    Functions to import transactions from csv file implemented
    This took way too long

--- CodeGoesHere/Budgeter/helpers.py
+++ CodeGoesHere/Budgeter/helpers.py
@@ -1,6 +1,7 @@
 import Budgeter.models as model
 from django.db import models
 import csv
+from decimal import *
 
 #helper functions to handle various data processing functions
 
@@ -15,16 +16,17 @@ def transactionFormatAndSave(rawTransactions):
 	for row in rawTransactions:
 		dateString = row['DATE']
 		tokenizedString = dateString.split('/')
-		if len(tokenizedString[0]) == 1:
-			formatedDay = '0'+tokenizedString[0]
-		else:
-			formatedDay = tokenizedString[0]
-
 		if len(tokenizedString[1]) == 1:
-			formatedMonth = '0'+tokenizedString[1]
+			formatedDay = '0'+tokenizedString[1]
 		else:
-			formatedMonth = tokenizedString[1]
+			formatedDay = tokenizedString[1]
+
+		if len(tokenizedString[0]) == 1:
+			formatedMonth = '0'+tokenizedString[0]
+		else:
+			formatedMonth = tokenizedString[0]
 		formatedDate = tokenizedString[2]+'-'+formatedMonth+'-'+formatedDay
-		newTransaction = model.Transaction(transaction_date = formatedDate, merchant_name = row['TRANSACTION DETAILS'], transaction_amount = row['DEBIT'])
-		newTransaction.save(force_insert = True)
+		if row['DEBIT'] != '':
+			newTransaction = model.Transaction(transaction_date = formatedDate, merchant_name = row['TRANSACTION DETAILS'], transaction_amount = Decimal(row['DEBIT']))
+			newTransaction.save(force_insert = True)
 
