7eef1c17b9ea7661bed7729aa86d0d93d1498e45
CarlBlacklock <carl.alex.blacklock@gmail.com>
2016-02-06 03:59:10 -0500

    should fix date importing

--- CodeGoesHere/Budgeter/helpers.py
+++ CodeGoesHere/Budgeter/helpers.py
@@ -13,6 +13,18 @@ def getTransactionsFromFile(path_to_file):
 
 def transactionFormatAndSave(rawTransactions):
 	for row in rawTransactions:
-		newTransaction = model.Transaction(transaction_date = row['DATE'], merchant_name = row['TRASACTION DETAILS'], transaction_amount = row['DEBIT'])
+		dateString = row['DATE']
+		tokenizedString = dateString.split('/')
+		if len(tokenizedString[0]) == 1:
+			formatedDay = '0'+tokenizedString[0]
+		else:
+			formatedDay = tokenizedString[0]
+
+		if len(tokenizedString[1]) == 1:
+			formatedMonth = '0'+tokenizedString[1]
+		else:
+			formatedMonth = tokenizedString[1]
+		formatedDate = tokenizedString[2]+'-'+formatedMonth+'-'+formatedDay
+		newTransaction = model.Transaction(transaction_date = formatedDate, merchant_name = row['TRANSACTION DETAILS'], transaction_amount = row['DEBIT'])
 		newTransaction.save(force_insert = True)
 
