40c61da0ba8fd7d7ff7865d90fcebb4a656a39ee
CarlBlacklock <carl.alex.blacklock@gmail.com>
2016-02-06 13:28:23 -0500

    FIXING ALL YO SHIT
    YO DOG I HEARD YOU LIKE COMMITS

---
+++ CodeGoesHere/SpendingAnalysis/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SpendinganalysisConfig(AppConfig):
+    name = 'SpendingAnalysis'
--- CodeGoesHere/SpendingAnalysis/views.py
+++ CodeGoesHere/SpendingAnalysis/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
 def index(request):
-	context = {}
-	return render(request, 'index.html')
+	return HttpResponse("Spending Analysis")
