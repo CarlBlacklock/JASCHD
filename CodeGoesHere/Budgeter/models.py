0cbf2b72b9fe25fa3a49271c1af036a019b7c256
CarlBlacklock <carl.alex.blacklock@gmail.com>
2016-02-05 23:04:44 -0500

    more setup stuff done :D
    WHEN WILL IT END!?!?!?!?

--- CodeGoesHere/Budgeter/models.py
+++ CodeGoesHere/Budgeter/models.py
@@ -1,17 +1,24 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
 
 class User(models.Model):
 	#to be filled in
+	def __str__(self):
+		return self.user_name
 	user_id = models.EmailField(max_length = 254, primary_key = True)
 	user_name = models.TextField()
 	user_bank_name = models.TextField()
 
 class Transaction(models.Model):
+	def __str__(self):
+		return self.merchant_name
 	TRANSACTION_TYPE_CHOICES = ()
 	transaction_date = models.DateField()
-	transaction_type = models.
+	transaction_type = models.TextField()
+	merchant_name = models.TextField()
+	transaction_amount = models.DecimalField(max_digits = 12, decimal_places = 2)
 
 
 class TransactionCatagories(models.Model):
@@ -23,5 +30,7 @@ class TransactionCatagories(models.Model):
 		('CLOTHING', 'Clothing'),
 		('TRANSPORTATION', 'Transportation')
 		)
+	def __str__(self):
+		return self.merchant_catagory
 	merchant_name = models.TextField()
 	merchant_catagory = models.TextField(choices = TRANSACTION_CATAGORY_CHOICES)
--- CodeGoesHere/CodeGoesHere/settings.py
+++ CodeGoesHere/CodeGoesHere/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Budgeter.apps.BudgeterConfig'
+    'Budgeter.apps.BudgeterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
