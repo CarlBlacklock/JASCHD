cf8c280a4366e79cc0f996579216df15a2a5a0b7
windh8 <windH800000000@gmail.com>
2016-02-07 06:54:46 -0800

    Update views.py

--- CodeGoesHere/SpendingAnalysis/views.py
+++ CodeGoesHere/SpendingAnalysis/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.http import HttpResponseRedirect
 from Budgeter import helpers
 
 
 def index(request):
 	#have to change the passed parameters
-	fromData = helpers.transactionCatagorization(request.user.get_username(), 1)
-
-	return render_to_response('.\Budgeter\'templates\spendanalysis.html', fromData)
-	#return render(request, "spendanalysis.html", fromData)
+	if request.user.is_authenticated():
+		fromData = helpers.transactionCatagorization(request.user.get_username(), 1)
+		#return render_to_response('.\Budgeter\'templates\spendanalysis.html', fromData)
+		return render(request, 'spendanalysis.html', fromData)
+	else:
+		HttpResponseRedirect("/login")
