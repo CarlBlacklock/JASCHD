bd2364b0db8a1adad7a11ac42969f7b4d6d8782a
CarlBlacklock <carl.alex.blacklock@gmail.com>
2016-02-07 13:24:10 -0500

    it works :D

--- CodeGoesHere/Budgeter/views.py
+++ CodeGoesHere/Budgeter/views.py
@@ -25,7 +25,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             new_user = form.save()
-            return HttpResponseRedirect("/")
+            return HttpResponseRedirect("/useranalysis")
     else:
         form = UserCreationForm()
     return render(request, "registration/register.html", {
